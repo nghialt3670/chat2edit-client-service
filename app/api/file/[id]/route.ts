16fdf2d371d5c0114bb80daa7cf83e9bb88c15dd
nghialt3670 <nghialt3670@gmail.com>
2024-09-03 11:01:40 +0700

    Refactor and add auth to file route

--- app/api/file/[id]/route.ts
+++ app/api/file/[id]/route.ts
@@ -1,27 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
 import mongoose from "mongoose";
-import { GRIDFS_FOR_MESSAGE_FILES_BUCKET_NAME } from "@/lib/configs/db";
-import connectToDatabase from "@/lib/mongo";
+import { MESSAGE_FILES_BUCKET_NAME } from "@/lib/configs/db";
+import connectToMongoDB from "@/lib/mongo";
 import { logError } from "@/lib/utils";
+import prisma from "@/lib/prisma";
+import { auth } from "@/auth";
 
 export async function GET(
   req: NextRequest,
   { params }: { params: { id: string } },
 ) {
   try {
-    await connectToDatabase();
+    const { id } = params;
+
+    const session = await auth();
+    const accountId = session?.user?.id;
+    if (!accountId) return NextResponse.json({}, { status: 401 });
+
+    await connectToMongoDB();
     const db = mongoose.connection.db;
     if (!db) throw new Error("Error connect to MongoDB");
+    const bucketName = MESSAGE_FILES_BUCKET_NAME;
+    const bucket = new mongoose.mongo.GridFSBucket(db, { bucketName });
 
-    const bucket = new mongoose.mongo.GridFSBucket(db, {
-      bucketName: GRIDFS_FOR_MESSAGE_FILES_BUCKET_NAME,
+    const attachment = await prisma.attachment.findFirst({
+      where: { fileId: id },
     });
+    if (!attachment) return NextResponse.json({}, { status: 404 });
+    if (attachment.accountId !== accountId)
+      return NextResponse.json({}, { status: 403 });
 
-    const fileId = new mongoose.Types.ObjectId(params.id);
-    const fileDoc = await db.collection("files.files").findOne({ _id: fileId });
-
-    if (!fileDoc)
-      return NextResponse.json({ error: "File not found" }, { status: 404 });
+    const fileId = new mongoose.Types.ObjectId(id);
+    const fileDoc = await db
+      .collection(`${bucketName}.files`)
+      .findOne({ _id: fileId });
+    if (!fileDoc) return NextResponse.json({}, { status: 404 });
 
     const readStream = bucket.openDownloadStream(fileId);
     const readableStream = new ReadableStream({
